67e77929c0f3330e4cc18adf88659ff1edcd2861
VCDhyan <dhyanvaradha2005@gmail.com>
2025-03-13 11:05:26 +0530

    yes

--- Backend/src/Middleware/auth.js
+++ Backend/src/Middleware/auth.js
@@ -1,3 +1,7 @@
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+    const tokenauth = req.headers.authorization;
+}
